7277c0150f6f1a44653fa6a4c9e5c833ae2cc6ac
Thotino GOBIN-GANSOU <thotino@lalalab.com>
2022-05-29 18:13:51 +0200

    doc: add some comments

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ROMAN-NUMERAL-CONVERTER-SSE-APP
-This app aims to convert decimal numbers inot roman numerals.
+This app aims to convert decimal numbers into roman numerals.
 
 ## USE
-Open the `index.html` file in your browser
+This app need to be used with this back-end [repository](https://github.com/thotino/roman-numeral-converter-sse).
+Make sure that the server is launched and is listening on the port 3000.
+
+Open the `index.html` file in your browser.
+Click on the button to close the connection with the server.
--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 const button = document.querySelector('button')
 const eventList = document.querySelector('ul')
 
+// Open the persistent connection to the local server
 const host = 'http://localhost:3000/events'
 const es = new EventSource(host)
 
-
+// The connection event
 es.onopen = () => { console.log('The connection has been established.') }
+
+// The new incoming event
 es.onmessage = (event) => {
     console.log(event.data)
-    document.querySelector('#content').innerHTML = event.data;
+    const { emittedNumber, convertedNumber } = JSON.parse(event.data)
+    document.querySelector('#content').innerHTML = `The conversion of ${emittedNumber} is ${convertedNumber}`;
 }
+
+// The connection failure event
 es.onerror = () => { console.log('The connection has failed.') }
 
+// Close the connection
 button.onclick = () => {
     console.log('The connection has been closed.')
     es.close()
